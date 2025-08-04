f7609e3361bb6b6fc5d8feb26f61cf27165117b7
Joan Odicio <yusuke.joan@gmail.com>
2025-08-04 15:16:16 +0200

    Update db path

--- src/database/appDataSource.ts
+++ src/database/appDataSource.ts
@@ -2,9 +2,11 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { PreviewApp } from "../models/previewApp";
 
+const dbPath = process.env.DB_PATH || "db.sqlite";
+
 export const AppDataSource = new DataSource({
 	type: "sqlite",
-	database: "db.sqlite",
-	synchronize: true,
+	database: dbPath,
+	synchronize: process.env.NODE_ENV !== "prod",
 	entities: [PreviewApp],
 });
